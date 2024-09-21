af9fd8bbfd966b47c338047fa8f3931b26967f5a
Adrian Mørbán <46272302+adrianmorban@users.noreply.github.com>
2024-09-21 17:00:37 -0400

    Add .gitignore and package.json files
    - Added .gitignore file to exclude node_modules directory from version control.
    - Added package.json file with a dependency on "@aws-sdk/client-sfn" version 3.654.0.

---
+++ index.js
@@ -0,0 +1,22 @@
+import { SFNClient, StartExecutionCommand } from "@aws-sdk/client-sfn";
+
+const client = new SFNClient({ region: 'us-east-1' });
+const stateMachineARN = process.env.STATE_MACHINE_ARN;
+
+const start = (event) => {
+    try{
+        const { messages } = event;
+        const params = {
+            stateMachineArn: stateMachineARN,
+            input: JSON.stringify({ messages })
+        };
+        const command = new StartExecutionCommand(params);
+        return client.send(command);
+    }
+    catch(e){
+        console.error("Error: ", e);
+        throw e;
+    }
+}
+
+export default start;
